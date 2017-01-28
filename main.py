3193b72bb267b8b9bb194d17ebddf33334655bd9
Chao <chaoguo94@gmail.com>
2017-01-28 16:44:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import requests
 import unicodecsv as csv
-#import csv
 from bs4 import BeautifulSoup
 
-# feed_url = "http://thegradcafe.com/survey/index.php"
-feed_url = "http://thegradcafe.com/survey/index.php?t=m&o=&p=3"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+def main(major,page_num):
+	feed_url = "http://thegradcafe.com/survey/index.php?q="+major+"&t=a&o=&p="+str(page_num)
 
-results_holder = [0]*5
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-# Get page source and parse it
-r = requests.get(feed_url, headers=headers)
-page_source = r.text
-soup = BeautifulSoup(page_source,"html.parser")
+	results_holder = [0]*5
 
-# Look for the results in page source
-with open("results.csv","wb") as csvfile:
-	admission_results_writer = csv.writer(csvfile,delimiter="\t")
-	for i in soup.find_all("tr",["row0","row1"]):
-		for j in i:
-			results_holder.append(j.get_text())
-		admission_results_writer.writerow(results_holder)
-		results_holder = []
+	# Get page source and parse it
+	r = requests.get(feed_url, headers=headers)
+	page_source = r.text
+	soup = BeautifulSoup(page_source,"html.parser")
+
+	# Look for the results in page source and write to csv
+	with open("results.csv","a") as csvfile:
+		admission_results_writer = csv.writer(csvfile,delimiter=",")
+		for i in soup.find_all("tr",["row0","row1"]):
+			for j in i:
+				results_holder.append(j.get_text())
+			if len(results_holder)==11: # some results have [0,0,0,0,0] in front of it. Reason unknown, yet
+				admission_results_writer.writerow(results_holder[5:-1])
+			else:
+				admission_results_writer.writerow(results_holder[:-1])
+			results_holder = []
+
+if __name__=='__main__':
+	major_list = raw_input("Enter Majors, separated by comma:")
+	for major in major_list.split(","):
+		for page_num in range(1,7):
+			main(major,str(page_num))
